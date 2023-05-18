af3af15ae2cb8ab91ba21294360b82625689d2a1
IgnacioCamIMT <43856051+IgnacioCamIMT@users.noreply.github.com>
2023-05-17 21:53:51 -0400

    Add files via upload

--- DF CODE/code.js
+++ DF CODE/code.js
@@ -51,3 +51,66 @@ function menorAyB() {
         }
     }
 }
+
+function obtenerDiv() {
+    var A = parseInt(prompt("Ingrese A:", 10));
+    var B = parseInt(prompt("Ingrese B:", 7));
+    var resultado = ~~(A / B);
+
+    alert(resultado);
+}
+
+function obtenerMod() {
+    var A = parseInt(prompt("Ingrese A:", 10));
+    var B = parseInt(prompt("Ingrese B:", 7));
+    var resultado = A % B;
+
+    alert(resultado);
+}
+
+function digitosN() {
+    var N = parseInt(prompt("Ingresar N", 3));
+    var resp = N;
+    var cantidad = 1;
+
+    if (N == 0) {
+        cantidad = 1;
+        alert(cantidad + " dígitos");
+    } else {
+        while (N != 0) {
+            N = parseInt(N / 10);
+            cantidad += 1;
+        }
+        alert(cantidad + " dígitos");
+    }
+}
+
+function retornarDigitosN() {
+    var N = parseInt(prompt("Ingresar N", 3));
+    var dig = 0;
+
+    if (N == 0) {
+        dig = N;
+        alert(dig);
+    } else {
+        while (N != 0) {
+            dig = N % 10;
+            alert(dig);
+            N = parseInt(N / 10);
+        }
+    }
+}
+
+function mostrarTres() {
+    var N = parseInt(prompt("Ingresar N", 3));
+    var dig = 0;
+
+    while (N != 0) {
+        dig = N % 10;
+        if (dig == 3) {
+            alert("Existe el número 3");
+            break
+        }
+        N = parseInt(N / 10);
+    }
+}
