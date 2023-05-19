5d445dc3bbe31194d69cefa68ac46daeec55fd34
IgnacioCamIMT <43856051+IgnacioCamIMT@users.noreply.github.com>
2023-05-18 21:49:51 -0400

    Add files via upload

--- DF code ING/code.js
+++ DF code ING/code.js
@@ -64,7 +64,6 @@ function obtenerMod() {
 
 function obtenerCantidadDigN() {
     var N = parseInt(prompt("Ingrese N", 9478324))
-    var resp = 0
     var cantidad = 0
     while (N != 0) {
         N = parseInt(N / 10)
@@ -88,10 +87,65 @@ function existeDig3enN() {
     var dig = 0
     while (N != 0) {
         dig = parseInt(N % 10)
-        if(dig == 3){
+        if (dig == 3) {
             alert("Existe el dig 3")
         }
         N = parseInt(N / 10)
     }
 }
 
+function calcularTrapecio() {
+    var h = parseInt(prompt("Ingrese h", 10))
+    var b = parseInt(prompt("Ingrese b", 5))
+    var B = parseInt(prompt("Ingrese B", 15))
+    var a = parseInt(prompt("Ingrese a", 10))
+    var c = parseInt(prompt("Ingrese c", 7))
+    var area = 0
+    var perimetro = 0
+    area = ((b + B) / 2) * h
+    perimetro = a + b + B + c
+    alert("El area es: " + area)
+    alert("El perimetro es: " + perimetro)
+}
+
+function determinarNesParImpar() {
+    var N = parseInt(prompt("Ingrese N"))
+    if ((N % 2) == 0) {
+        alert(N + " es par.")
+    } else {
+        alert(N + " es impar.")
+    }
+}
+
+function ifNormal() {
+    var N = parseInt(prompt("Ingrese N"))
+    if (N > 5) {
+        N = 10
+    } else {
+        N = 1
+    }
+    alert(N)
+}
+
+function ifAbreviado() {
+    var N = parseInt(prompt("Ingrese N"))
+    N = (N > 5) ? 10 : 1
+    alert(N)
+}
+/*
+    var N = parseInt(prompt("Ingrese N", 9478324))
+    var cantidad = 0
+    while (N != 0) {
+        N = parseInt(N / 10)
+        cantidad = cantidad + 1
+    }
+    alert("digitos: " + cantidad)
+*/
+function obtenerCantidadDigN_FOR() {
+    var N = parseInt(prompt("Ingrese N", 9478324))
+    var cantidad
+    for (cantidad = 0; N != 0; cantidad++) {
+        N = parseInt(N / 10)
+    }
+    alert("digitos: " + cantidad)
+}
